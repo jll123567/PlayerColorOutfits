bee1e01c3bd80741bb3c924b1c9f82dc483c9f01
JillTheSomething <ledbetterte@gmail.com>
2025-12-26 15:02:28 -0500

    Version Bump

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using MelonLoader;
 
-[assembly: MelonInfo(typeof(PlayerColorOutfits.Main), "PlayerColorOutfits", "1.1.0", "JillTheSomething", "")]
+[assembly: MelonInfo(typeof(PlayerColorOutfits.Main), "PlayerColorOutfits", "1.2.0", "JillTheSomething", "")]
 [assembly: MelonGame("ChilloutVR", "ChilloutVR")]
 [assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
 [assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
